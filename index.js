b2bd3a6a09e98956d325bc7ddabebd92a0922a68
Francois Campbell <campbell.francois@gmail.com>
2018-05-25 22:53:25 -0400

    remove unused function

--- index.js
+++ index.js
@@ -4,34 +4,6 @@ function invariant(condition, errorMessage) {
 	}
 }
 
-function getOptions(providedOptions) {
-	const smsTo = options.smsTo
-	invariant(Array.isArray(smsTo), 'options.smsTo must be an Array')
-
-	const smsFrom = options.smsFrom
-	invariant(!!smsTo === !!smsFrom, 'options.smsTo and options.smsFrom must both be specified')
-
-	const smsText = options.smsText || "🚪 There's someone at the door 🚪"
-	
-	const unlockDigits = options.unlockDigits
-	invariant(typeof unlockDigits === 'string', 'options.unlockDigits must be a string')
-	
-	if (smsFrom && smsTo && smsText) {
-		return {
-			sms: {
-				from: smsFrom,
-				text: smsText,
-				to: smsTo,
-			},
-			unlockDigits
-		}
-	}
-
-	return {
-		unlockDigits
-	}
-}
-
 function dial(digits) {
     const response = new Twilio.twiml.VoiceResponse()
 	response.play({ digits })
@@ -55,7 +27,7 @@ module.exports = function(options) {
 	const smsOptions = options.smsTo
 	if (smsOptions) {
 		invariant(Array.isArray(sms.to), 'options.sms.to must be an Array')
-		invariant(!!smsTo === !!smsFrom, 'options.sms.to and options.sms.from must both be specified')
+		invariant(!smsTo === !smsFrom, 'options.sms.to and options.sms.from must both be specified')
 		const smsText = options.sms.text || "🚪 There's someone at the door 🚪"
 		return function(context, event, callback) {
 		    const client = context.getTwilioClient()
