aa041e2358d601ca47ff51532aa6adc2e209f8cc
Francois Campbell <campbell.francois@gmail.com>
2018-05-25 22:56:36 -0400

    update readme, description, and move default sms text

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # apartmentwilio
-Apartment front door automation
+Apartment lobby door automation
+
+Use this as a [Twilio serverless function](https://www.twilio.com/console/runtime/functions/manage)
+
+```js
+exports.handler = apartmentwilio({
+	sms: {
+		from: '+1234567890'  // Your Twilio to send from
+		to: ['+1234567890']  // Numbers to send to,
+		text: 'From door buzzer opened'  // Text to send as SMS (optional; defaults to "🚪 There's someone at the door 🚪")
+	},
+	unlockDigits: '9'  // The number you'd dial if you were unlocking the lobby door manually
+})
+```
+
+
--- index.js
+++ index.js
@@ -20,6 +20,8 @@ function sendSms(client, body, from, tos) {
     )
 }
 
+const defaultSmsText = "🚪 There's someone at the door 🚪"
+
 module.exports = function(options) {
 	const unlockDigits = options.unlockDigits
 	invariant(typeof unlockDigits === 'string', 'options.unlockDigits must be a string')
@@ -28,7 +30,7 @@ module.exports = function(options) {
 	if (smsOptions) {
 		invariant(Array.isArray(sms.to), 'options.sms.to must be an Array')
 		invariant(!smsTo === !smsFrom, 'options.sms.to and options.sms.from must both be specified')
-		const smsText = options.sms.text || "🚪 There's someone at the door 🚪"
+		const smsText = options.sms.text || defaultSmsText
 		return function(context, event, callback) {
 		    const client = context.getTwilioClient()
 		    sendSms(client, smsText, sms.from, sms.to)
